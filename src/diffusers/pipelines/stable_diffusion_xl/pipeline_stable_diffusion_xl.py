e0012162c9db49f5138bdf918bd3186151274dfa
mahdi-shafee <82397556+mahdi-shafee@users.noreply.github.com>
2023-12-04 03:07:21 +0330

    Update pipeline_stable_diffusion_xl.py

--- src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl.py
+++ src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl.py
@@ -1305,14 +1305,14 @@ class StableDiffusionXLPipeline(
             
             latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs, return_dict=False)[0]
 
-        with torch.no_grad():
-            self.upcast_vae()
-            latents = latents.to(next(iter(self.vae.post_quant_conv.parameters())).dtype)
-            test_image = self.vae.decode(latents / self.vae.config.scaling_factor, return_dict=False)[0]
-            image = test_image
-            image = self.image_processor.postprocess(image, output_type="pil")
-            image = StableDiffusionXLPipelineOutput(images=image)
-            self.vae.to(dtype=torch.float16)
+        self.vae.requires_grad_(False)
+        self.upcast_vae()
+        latents = latents.to(next(iter(self.vae.post_quant_conv.parameters())).dtype)
+        test_image = self.vae.decode(latents / self.vae.config.scaling_factor, return_dict=False)[0]
+        image = test_image
+        image = self.image_processor.postprocess(image, output_type="pil")
+        image = StableDiffusionXLPipelineOutput(images=image)
+        self.vae.to(dtype=torch.float16)
 
         print(latents)
         return image
