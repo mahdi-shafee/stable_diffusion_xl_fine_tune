1a9d92c2bd61a2202e62e8d845b9b394f56e317d
mahdi-shafee <82397556+mahdi-shafee@users.noreply.github.com>
2023-12-03 22:52:17 +0330

    Update pipeline_stable_diffusion_xl.py

--- src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl.py
+++ src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl.py
@@ -1291,9 +1291,8 @@ class StableDiffusionXLPipeline(
                     return_dict=False,
                 )[0]
 
-            if self.do_classifier_free_guidance:
-                noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
-                noise_pred = noise_pred_uncond + self.guidance_scale * (noise_pred_text - noise_pred_uncond)
+            noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+            noise_pred = noise_pred_uncond + self.guidance_scale * (noise_pred_text - noise_pred_uncond)
             
             latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs, return_dict=False)[0]
             print(latents.shape)
