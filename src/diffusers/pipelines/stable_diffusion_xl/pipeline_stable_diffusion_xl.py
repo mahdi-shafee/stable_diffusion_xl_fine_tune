de912f5d650b4609b98ed484474ddd2b27d2b3aa
mahdi-shafee <82397556+mahdi-shafee@users.noreply.github.com>
2023-12-03 23:10:17 +0330

    Update pipeline_stable_diffusion_xl.py

--- src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl.py
+++ src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl.py
@@ -1297,9 +1297,9 @@ class StableDiffusionXLPipeline(
             latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs, return_dict=False)[0]
             print(latents.shape)
 
+        self.upcast_vae()
         latents = latents.to(next(iter(self.vae.post_quant_conv.parameters())).dtype)
-        with torch.no_grad():
-            image = self.vae.decode(latents / self.vae.config.scaling_factor, return_dict=False)[0]
+        image = self.vae.decode(latents / self.vae.config.scaling_factor, return_dict=False)[0]
+        image = self.image_processor.postprocess(image, output_type=output_type)
+        image = StableDiffusionXLPipelineOutput(images=image)
         self.vae.to(dtype=torch.float16)
-        image = self.image_processor.postprocess(image, output_type="pil")
-        return StableDiffusionXLPipelineOutput(images=image)
